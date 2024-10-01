322b3558d36d4980a2c6bbc0ca231898d7d42966
leeeunseo-1 <vdldmstj0909@naver.com>
2024-10-02 06:00:32 +0900

    개인

---
+++ 개인/javascript.js
@@ -0,0 +1,18 @@
+console.log("word");
+console.log('word');
+console.log(`word`);
+console.log("This is sentence");
+console.log("3");
+console.log(3);
+// <여닫이 부호는 3가지>
+// 큰 따옴표"", 작은 따옴표'', 벡틱``
+// "3" 과 3 의 결과값은 모두 3이다. 그러나 "3"은 문자열이고, 3은 숫자이다.
+
+console.log("I'll Use a special Character");
+console.log(`I'll Use a special Character`); 
+console.log('I\'ll Use special Characters');
+// <문자열 내에 특수문자가 있는 경우>
+// 여닫이 부호를 3가지 중 하나도 겹치지 않는 것을 사용하거나,
+// 문자열의 특수문자 앞에 \를 기입한다.
+
+//  split() : 문자열을 특정 기준으로 잘라주는 함수
